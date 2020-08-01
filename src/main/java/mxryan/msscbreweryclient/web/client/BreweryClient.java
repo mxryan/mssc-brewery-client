79fd49f694b708f81e584ca04ad9608fb04d230d
Michael Ryan <mxryan@live.com>
2020-08-01 13:36:36 -0400

    initial commit

---
+++ src/main/java/mxryan/msscbreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,37 @@
+package mxryan.msscbreweryclient.web.client;
+
+import mxryan.msscbreweryclient.web.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.UUID;
+
+@Component
+@ConfigurationProperties(value="sfg.brewery", ignoreUnknownFields = false)
+public class BreweryClient {
+    public final String BEER_PATH_V1 = "/api/v1/beer/";
+
+    public void setApihost(String apihost) {
+        this.apihost = apihost;
+    }
+
+    private String apihost;
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID uuid) {
+        return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+    }
+
+    public URI saveNewBeer(BeerDto beerDto) {
+        System.out.println("aayyy apihost: " + apihost);
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
+    }
+
+ }
---
+++ src/test/java/mxryan/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,34 @@
+package mxryan.msscbreweryclient.web.client;
+
+import mxryan.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto beer = client.getBeerById(UUID.randomUUID());
+        assertNotNull(beer);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        // given
+        BeerDto beerDto = BeerDto.builder().beerName("La Farquada").build();
+        URI uri = client.saveNewBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+
+    }
+}
